41f4199a318139c74def7b252dfb3ed3ecf2051d
Us Nickolay <mr.us.nick@gmail.com>
2020-02-24 19:31:16 +0300

    getCanonicalPath() normalize()

--- src/FilePathDoublepointPoint.java
+++ src/FilePathDoublepointPoint.java
@@ -11,20 +11,30 @@
 //        a\..\b\c\file.txt
 
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.*;
 
 public class FilePathDoublepointPoint {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String s1 =     "C:\\Temp\\a\\b\\..\\file.txt";
         String s2 =     "C:\\Temp\\.\\a\\b\\..\\b\\c\\.\\file.txt";
         String s3 =     "C:\\Temp\\a\\b\\c\\file.txt";
         String s4 =     "C:\\Temp\\a\\.\\b\\..\\c\\.\\file.txt";
         String s5 =     "C:\\Temp\\a\\..\\b\\c\\file.txt";
         String [] s = {s1,s2,s3,s4,s5};
+
+
         for (String path : s) {
             Path file = Paths.get (path);
             System.out.printf("Для пути %s верна следующая нормализация %s%n",path, file.normalize());
+
+        }
+        System.out.println();
+        for (String path : s) {
+            File ff = new File(path);
+            System.out.printf("Для пути %s верна следующая нормализация %s%n",path, ff.getCanonicalPath());
 
         }
 
